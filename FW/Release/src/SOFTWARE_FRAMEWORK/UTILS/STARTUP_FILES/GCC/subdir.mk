77a0eb9f38306cda0a13fc751e2adba3c95f0779
Martin Stejskal <martin.stej@gmail.com>
2014-08-14 17:11:55 +0200

    Makefiles changed. Now optimizations and debug level is just a parameter   in main makefile
    2DO: Modify board driver functions and descriptions for generic driver
         Persistant changes for board driver (include TLV settings)
         Bootloader

--- FW/Release/src/SOFTWARE_FRAMEWORK/UTILS/STARTUP_FILES/GCC/subdir.mk
+++ FW/Release/src/SOFTWARE_FRAMEWORK/UTILS/STARTUP_FILES/GCC/subdir.mk
@@ -1,7 +1,3 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
 # Add inputs and outputs from these tool invocations to the build variables 
 X_SRCS += \
 ../src/SOFTWARE_FRAMEWORK/UTILS/STARTUP_FILES/GCC/crt0.x 
